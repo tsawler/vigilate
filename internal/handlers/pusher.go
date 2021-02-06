d106c1c6fe355b1fdc4ef6177721601ba899b4f3
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-06 19:23:38 -0400

    Remove pointless auth check

--- internal/handlers/pusher.go
+++ internal/handlers/pusher.go
@@ -1,10 +1,8 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/pusher/pusher-http-go"
 	"github.com/tsawler/vigilate/internal/config"
-	"github.com/tsawler/vigilate/internal/helpers"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -14,30 +12,25 @@ import (
 // PusherAuth authenticates for pusher
 func (repo *DBRepo) PusherAuth(app config.AppConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if !helpers.IsAuthenticated(r) {
-			w.WriteHeader(http.StatusForbidden)
-			_, _ = fmt.Fprint(w, "403 Access denied")
-		} else {
-			firstName := app.Session.GetString(r.Context(), "userName")
-			userID := app.Session.GetInt(r.Context(), "userID")
-			params, _ := ioutil.ReadAll(r.Body)
+		firstName := app.Session.GetString(r.Context(), "userName")
+		userID := app.Session.GetInt(r.Context(), "userID")
+		params, _ := ioutil.ReadAll(r.Body)
 
-			presenceData := pusher.MemberData{
-				UserID: strconv.Itoa(userID),
-				UserInfo: map[string]string{
-					"name": firstName,
-					"id":   strconv.Itoa(userID),
-				},
-			}
-
-			response, err := app.WsClient.AuthenticatePresenceChannel(params, presenceData)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			_, _ = w.Write(response)
+		presenceData := pusher.MemberData{
+			UserID: strconv.Itoa(userID),
+			UserInfo: map[string]string{
+				"name": firstName,
+				"id":   strconv.Itoa(userID),
+			},
 		}
+
+		response, err := app.WsClient.AuthenticatePresenceChannel(params, presenceData)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(response)
 	}
 }
