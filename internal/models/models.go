e61e4bc2a388e6f78d052b336d699cff0e5024e8
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-15 11:41:04 -0400

    Create models; add column to host_services

--- internal/models/models.go
+++ internal/models/models.go
@@ -39,3 +39,42 @@ type Preference struct {
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
+
+// Host is the model for hosts
+type Host struct {
+	ID            int
+	HostName      string
+	CanonicalName string
+	URL           string
+	IP            string
+	IPV6          string
+	Location      string
+	OS            string
+	Active        int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+// Services is the model for services
+type Services struct {
+	ID          int
+	ServiceName string
+	Active      int
+	Icon        string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// HostService is the model for host services
+type HostService struct {
+	ID             int
+	HostID         int
+	ServiceID      int
+	Active         int
+	ScheduleNumber int
+	ScheduleUnit   string
+	Status         string
+	LastCheck      time.Time
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
