203cf48391ca4cb19d8a3cf75d8e50958df7e41a
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-23 19:28:45 -0400

    cleanup

--- internal/repository/dbrepo/host_postgresql.go
+++ internal/repository/dbrepo/host_postgresql.go
@@ -38,8 +38,7 @@ func (m *postgresDBRepo) InsertHost(h models.Host) (int, error) {
 	// add host services and set to inactive
 	stmt := `
 		insert into host_services (host_id, service_id, active, schedule_number, schedule_unit,
-		status, created_at, updated_at) values ($1, 1, 0, 3, 'm', 'pending', $2, $3)
-`
+		status, created_at, updated_at) values ($1, 1, 0, 3, 'm', 'pending', $2, $3)`
 
 	_, err = m.DB.ExecContext(ctx, stmt, newID, time.Now(), time.Now())
 	if err != nil {
@@ -55,9 +54,10 @@ func (m *postgresDBRepo) GetHostByID(id int) (models.Host, error) {
 	defer cancel()
 
 	query := `
-		select id, host_name, canonical_name, url, ip, ipv6, location, os, active, created_at, updated_at
-		from hosts where id = $1
-`
+		select 
+			id, host_name, canonical_name, url, ip, ipv6, location, os, active, created_at, updated_at
+		from 
+			hosts where id = $1`
 
 	row := m.DB.QueryRowContext(ctx, query, id)
 
@@ -82,16 +82,16 @@ func (m *postgresDBRepo) GetHostByID(id int) (models.Host, error) {
 	}
 
 	// get all services for host
-	query = `select 
-       				hs.id, hs.host_id, hs.service_id, hs.active, hs.schedule_number, hs.schedule_unit, 
-					hs.last_check, hs.status, hs.created_at, hs.updated_at,
-       				s.id, s.service_name, s.active, s.icon, s.created_at, s.updated_at
-				from 
-				    host_services hs
-					left join services s on (s.id = hs.service_id)
-				where
-					host_id = $1
-`
+	query = `
+			select 
+				hs.id, hs.host_id, hs.service_id, hs.active, hs.schedule_number, hs.schedule_unit, 
+				hs.last_check, hs.status, hs.created_at, hs.updated_at,
+				s.id, s.service_name, s.active, s.icon, s.created_at, s.updated_at
+			from 
+				host_services hs
+				left join services s on (s.id = hs.service_id)
+			where
+				host_id = $1`
 
 	rows, err := m.DB.QueryContext(ctx, query, h.ID)
 	if err != nil {
@@ -139,9 +139,13 @@ func (m *postgresDBRepo) UpdateHost(h models.Host) error {
 	defer cancel()
 
 	stmt := `
-		update hosts set host_name = $1, canonical_name = $2, url = $3, ip = $4, ipv6 = $5, os = $6,
-		active = $7, location = $8, updated_at = $9 where id = $10
-`
+			update 
+    			hosts 
+			set 
+			    host_name = $1, canonical_name = $2, url = $3, ip = $4, ipv6 = $5, os = $6,
+				active = $7, location = $8, updated_at = $9 
+			where 
+			    id = $10`
 
 	_, err := m.DB.ExecContext(ctx, stmt,
 		h.HostName,
@@ -170,9 +174,13 @@ func (m *postgresDBRepo) AllHosts() ([]models.Host, error) {
 	defer cancel()
 
 	query := `
-		select id, host_name, canonical_name, url, ip, ipv6, location, os,
-		active, created_at, updated_at from hosts order by host_name
-`
+			select 
+       			id, host_name, canonical_name, url, ip, ipv6, location, os,
+				active, created_at, updated_at 
+			from 
+			     hosts 
+			order by 
+				host_name`
 
 	rows, err := m.DB.QueryContext(ctx, query)
 	if err != nil {
