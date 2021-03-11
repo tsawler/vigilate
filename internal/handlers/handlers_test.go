b1cdf12d64237405182e02cf7fefaf8ed4bda4ed
Trevor Sawler <trevor.sawler@gmail.com>
2021-03-11 09:58:26 -0400

    Testing Handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -8,55 +8,73 @@ import (
 	"testing"
 )
 
-// TestDBRepo_LoginScreen tests showing the login screen
-func TestDBRepo_LoginScreen(t *testing.T) {
-	// create a request
-	req, _ := http.NewRequest("GET", "/", nil)
-
-	// add the session info to the context
-	ctx := getCtx(req)
-	req = req.WithContext(ctx)
-
-	// create a recorder
-	rr := httptest.NewRecorder()
-
-	// cast handler we want to test to an http.HandlerFunc
-	handler := http.HandlerFunc(Repo.LoginScreen)
-
-	// call the handler with our response recorder (which satisfies the response writer interface),
-	// and our request (which has our test session)
-	handler.ServeHTTP(rr, req)
-
-	// check returned status code against expectd status code
-	if rr.Code != http.StatusOK {
-		t.Errorf("failed login screen: expected 200, but got %s - %d", rr.Code)
-	}
+var loginTests = []struct {
+	name                 string
+	url                  string
+	method               string
+	postedData           url.Values
+	expectedResponseCode int
+}{
+	{
+		name:                 "login-screen",
+		url:                  "/",
+		method:               "POST",
+		expectedResponseCode: http.StatusOK,
+	},
+	{
+		name:   "login-screen-post",
+		url:    "/",
+		method: "POST",
+		postedData: url.Values{
+			"email":    {"me@here.com"},
+			"password": {"password"},
+		},
+		expectedResponseCode: http.StatusSeeOther,
+	},
 }
 
-// TestDBRepo_Login tests logging in
-func TestDBRepo_Login(t *testing.T) {
-	// put create url.Values with posted data
-	postedData := url.Values{
-		"email":    {"admin@example.com"},
-		"password": {"password"},
-	}
+func TestLoginScreen(t *testing.T) {
+	for _, e := range loginTests {
+		if e.method == "GET" {
+			// create a request
+			req, _ := http.NewRequest("GET", e.url, nil)
 
-	// create a request with body to post
-	req, _ := http.NewRequest("POST", "/", strings.NewReader(postedData.Encode()))
+			// add the session info to the context
+			ctx := getCtx(req)
+			req = req.WithContext(ctx)
 
-	// get our context with the session
-	ctx := getCtx(req)
-	req = req.WithContext(ctx)
+			// create a recorder
+			rr := httptest.NewRecorder()
 
-	// create a recorder
-	rr := httptest.NewRecorder()
+			// cast handler we want to test to an http.HandlerFunc
+			handler := http.HandlerFunc(Repo.LoginScreen)
+			// call the handler with our response recorder (which satisfies the response writer interface),
+			// and our request (which has our test session)
+			handler.ServeHTTP(rr, req)
 
-	// cast the handler to a handlerfunc and call serve http
-	handler := http.HandlerFunc(Repo.Login)
-	handler.ServeHTTP(rr, req)
+			// check returned status code against expected status code
+			if rr.Code != http.StatusOK {
+				t.Errorf("expected %d, but got %d", e.expectedResponseCode, rr.Code)
+			}
+		} else {
+			// create a request with body to post
+			req, _ := http.NewRequest("POST", "/", strings.NewReader(e.postedData.Encode()))
 
-	// test returned status code
-	if rr.Code != http.StatusSeeOther {
-		t.Errorf("failed post login screen: expected 303, but got %d", rr.Code)
+			// get our context with the session
+			ctx := getCtx(req)
+			req = req.WithContext(ctx)
+
+			// create a recorder
+			rr := httptest.NewRecorder()
+
+			// cast the handler to a handlerfunc and call serve http
+			handler := http.HandlerFunc(Repo.Login)
+			handler.ServeHTTP(rr, req)
+
+			// test returned status code
+			if rr.Code != http.StatusSeeOther {
+				t.Errorf("expected %d, but got %d", e.expectedResponseCode, rr.Code)
+			}
+		}
 	}
 }
